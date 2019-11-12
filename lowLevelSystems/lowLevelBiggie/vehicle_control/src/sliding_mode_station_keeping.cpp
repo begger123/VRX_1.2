98cdee89bfdb034aed9d764a63f9a776dd2c1ca7
Travis Moscicki <tmoscicki2013@fau.edu>
2019-11-12 07:23:09 -0800

    working on sm_sk

--- lowLevelSystems/lowLevelBiggie/vehicle_control/src/sliding_mode_station_keeping.cpp
+++ lowLevelSystems/lowLevelBiggie/vehicle_control/src/sliding_mode_station_keeping.cpp
@@ -21,7 +21,7 @@ sm_controller::sl_mode_st_keep::sl_mode_st_keep(ros::NodeHandle &nh) : sm_sk_nh_
    		ros::console::notifyLoggerLevelsChanged();
 	}
 	ROS_DEBUG("Entering initializer, next stop get_params");
-	target_sub_ = sm_sk_nh_->subscribe("control_target", 10, &sm_controller::sl_mode_st_keep::target_callback, this);
+	target_sub_ = sm_sk_nh_->subscribe("control_target_sk", 10, &sm_controller::sl_mode_st_keep::target_callback, this);
 	state_sub_ = sm_sk_nh_->subscribe("/p3d_wamv_ned", 10, &sm_controller::sl_mode_st_keep::state_callback, this);
 	control_effort_pub_ = sm_sk_nh_->advertise<custom_messages_biggie::control_effort>("/control_effort", 10); //published TAU = {X,Y,Eta}
 
